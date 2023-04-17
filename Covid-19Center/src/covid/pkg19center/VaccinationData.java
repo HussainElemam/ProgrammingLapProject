a76bd458188becab1d81ed3a9d7247a93b0b320f
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2023-04-17 12:09:08 +0400

    Add files via upload

---
+++ Covid-19Center/src/covid/pkg19center/VaccinationData.java
@@ -0,0 +1,13 @@
+package covid.pkg19center;
+
+import java.time.*;
+
+/**
+ *
+ * @author Hussain
+ */
+public class VaccinationData {
+    private Vaccine vaccine;
+    private LocalDate date;
+    
+}
